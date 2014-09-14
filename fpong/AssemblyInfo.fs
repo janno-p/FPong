51147756a29e8ad8dee8fa729aa426bf5f1825e2
Janno Põldma <janno.poldma@gmail.com>
2014-09-14 15:06:25 +0300

    Initial F# console application in MonoDevelop.

---
+++ fpong/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module fpong.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("fpong")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("janno")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
