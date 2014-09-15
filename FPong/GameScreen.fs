d972f171b143b9f57bb21c00661cb26b7e2215d1
Janno Põldma <janno.poldma@gmail.com>
2014-09-15 15:43:02 +0300

    Refactored to use asynchronous workflow. Removed sfmlnet-graphics-2.dll.config from tracking, because of different setups.

---
+++ FPong/GameScreen.fs
@@ -0,0 +1,17 @@
+﻿module FPong.GameScreen
+
+open SFML.Graphics
+open SFML.Window
+
+let run (window : RenderWindow) = async {
+    window.Closed.Add(fun _ -> window.Close())
+    window.KeyPressed.Add(fun e -> if e.Code = Keyboard.Key.Escape then window.Close())
+
+    use shape = new RectangleShape(Vector2f(75.0f, 50.0f), Position=Vector2f(100.0f, 100.0f), FillColor=Color.Green)
+
+    while window.IsOpen() do
+        window.DispatchEvents()
+        window.Clear()
+        window.Draw(shape)
+        window.Display()
+}
--- FPong/Program.fs
+++ FPong/Program.fs
@@ -2,20 +2,19 @@
 
 open SFML.Graphics
 open SFML.Window
-open FPong.AssemblyInfo
+open FPong
 
 [<EntryPoint>]
 let main argv =
-    use window = new RenderWindow(VideoMode(600u, 480u), (sprintf "FPong v.%s" assemblyVersion))
-    use shape = new RectangleShape(Vector2f(75.0f, 50.0f), Position=Vector2f(100.0f, 100.0f), FillColor=Color.Green)
+    let videoMode = VideoMode(600u, 480u)
+    let titleText = sprintf "FPong v.%s" AssemblyInfo.assemblyVersion
 
-    window.Closed.Add(fun _ -> window.Close())
-    window.KeyPressed.Add(fun e -> if e.Code = Keyboard.Key.Escape then window.Close())
+    use window = new RenderWindow(videoMode, titleText)
 
-    while window.IsOpen() do
-        window.DispatchEvents()
-        window.Clear()
-        window.Draw(shape)
-        window.Display()
+    window.SetVerticalSyncEnabled(true)
+
+    async {
+        do! GameScreen.run window
+    } |> Async.StartImmediate
 
     0
